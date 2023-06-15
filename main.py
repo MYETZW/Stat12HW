63fb33ade5a5580a4feae483facef8f626b9578b
MYETZW <mai@neopatent.com>
2023-06-15 16:45:51 -0700

    ignore .idea directory

--- main.py
+++ main.py
@@ -1,16 +1,32 @@
-# This is a sample Python script.
+import pandas as pd
+import statistics
+## import numpy as np ##
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+def loadData(filename):
+    frame = pd.read_csv(filename, header=None)
+    return frame
 
+def calculate_statistics(frame):
+    mean = frame.mean()
+    median = frame.median()
+    mode = statistics.mode(frame[0])  # statistics.mode() requires a 1D list, so we use frame[0]
+    maximum = frame.max()
+    minimum = frame.min()
+    quartile_25 = frame.quantile(0.25)
+    quartile_75 = frame.quantile(0.75)
 
-def print_hi(aname):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {aname}')  # Press Ctrl+F8 to toggle the breakpoint.
+    return mean, median, mode, maximum, minimum, quartile_25, quartile_75
 
+def main():
+    frame = loadData("1000_Random_Numbers.csv")
+    mean, median, mode, maximum, minimum, quartile_25, quartile_75 = calculate_statistics(frame)
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
+    print("Mean:", mean)
+    print("Median:", median)
+    print("Mode:", mode)
+    print("Maximum:", maximum)
+    print("Minimum:", minimum)
+    print("25% Quartile:", quartile_25)
+    print("75% Quartile:", quartile_75)
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+main()
